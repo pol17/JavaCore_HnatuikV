4970c3cda1feb6b8d554acd6451a8b6af710f660
Vasiliy_Gnatiuk <pol17@ukr.net>
2019-12-19 20:34:45 +0200

    runner update

--- src/lesson4/Runner.java
+++ src/lesson4/Runner.java
@@ -4,7 +4,6 @@ public class Runner {
     public static void main(String[] args) {
         Table table = new Table();
         System.out.println(table.a);
-
         System.out.println(MyExceptionMessages.NOT_SUPPORTED_OPERATION);
     }
 }
